9d9a97b3ed4b049dfe8eb9ba345a0b108fa02fdd
c.lamboo <casperlamboo@gmail.com>
2022-07-18 13:37:23 +0200

    Revert "Revert "Specify channel for conandata""
    This reverts commit 4cc5c3a54b0826ee2f59f8bb4291bff2522d0e2d.

--- generators/PyCharmRunEnv.py
+++ generators/PyCharmRunEnv.py
@@ -35,7 +35,7 @@ class PyCharmRunEnv(Generator):
 
     @property
     def filename(self):
-        stem = Path(self.conanfile._um_data(self.conanfile.version)["runinfo"]["entrypoint"]).stem
+        stem = Path(self.conanfile._um_data(self.conanfile.version, self.channel)["runinfo"]["entrypoint"]).stem
         return str(Path(self.conanfile.source_folder).joinpath(".run", f"{stem}.run.xml"))
 
     @property
@@ -43,6 +43,6 @@ class PyCharmRunEnv(Generator):
         run_env = VirtualRunEnv(self.conanfile)
         env = run_env.environment()
         envvars = env.vars(self.conanfile, scope = "run")
-        entrypoint = self.conanfile._um_data(self.conanfile.version)["runinfo"]["entrypoint"]
+        entrypoint = self.conanfile._um_data(self.conanfile.version, self.channel)["runinfo"]["entrypoint"]
         return self.run_xml.render(name = entrypoint, envvars = envvars,
                                    entrypoint = entrypoint)
